a47c73cefbeb6b1275dc3b5bd1f7cdd4b3e6c6e1
ClanceyHuang <clanceyhuang@gmail.com>
2018-06-14 08:50:18 +0800

    系统日志

---
+++ core/class/WebLog.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirk
+ * Date: 18-6-12
+ * Time: 上午10:36
+ */
+class WebLog extends Log{
+    
+}
--- core/config/log.php
+++ core/config/log.php
@@ -11,8 +11,8 @@ define("ROOT_PATH",dirname(dirname(dirname((__FILE__)))) . '/');
 
 # log日志相关的配置信息
 $config['log'] = array(
-    'DRIVE' => 'file',
+    'DRIVE' => 'file',  # file:文件模式记录日志  mysql:数据库模式记录日志
 	'OPTION' => array(
-	    'PATH'=>ROOT_PATH.'log/'
+	    'PATH'=>ROOT_PATH.'log/'  # 文件模式下的日志路径
     )
 );
