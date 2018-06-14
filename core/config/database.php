aaef82e9ae38ef7778e413de2aa99eb675611198
ClanceyHuang <clanceyhuang@gmail.com>
2018-06-14 10:32:59 +0800

    优化

--- core/config/database.php
+++ core/config/database.php
@@ -1,22 +1,4 @@
 <?php
-//===============================V1.0 版本===========已废弃============
-//return array(
-//	'DSN' => 'mysql:host=localhost;dbname=dbname',
-//	'USERNAME' => '',
-//	'PASSWD' => ''
-//);
-
-//# 改为medoo的配置
-//return array(
-//    'database_type' => '',
-//    'database_name' => '',
-//    'server' => '',
-//    'username' => '',
-//    'password' => ''
-//);
-//===============================V1.0 版本===========已废弃============
-
-
 # 数据库相关的配置信息
 $config['mongodb'][''] = array(
     'dbname' => '',
--- core/config/log.php
+++ core/config/log.php
@@ -1,14 +1,5 @@
 <?php
 define("ROOT_PATH",dirname(dirname(dirname((__FILE__)))) . '/');
-//===============================V1.0 版本===========已废弃============
-//return array(
-//	'DRIVE' => 'file',
-//	'OPTION' => array(
-//		'PATH'=>KIRK.'/log/'
-//	)
-//);
-//===============================V1.0 版本===========已废弃============
-
 # log日志相关的配置信息
 $config['log'] = array(
     'DRIVE' => 'file',  # file:文件模式记录日志  mysql:数据库模式记录日志
