a2953bc0b40f6894751736881ab7336115166510
ClanceyHuang <clanceyhuang@gmail.com>
2018-06-19 18:08:55 +0800

    IP白名单设计

--- app-home/class/HomeRequest.php
+++ app-home/class/HomeRequest.php
@@ -8,5 +8,42 @@
 kirk_require_class('WebRequest');
 
 class HomeRequest extends WebRequest{
+    public function check_ip_black($exit = true){
+        $ip = $this->get_client_ip();
+        $user_agent = $this->get_user_agent();
+        // 验证ＩＰ是否在白名单中
+        if ($this->is_white_ip($ip)){
+            return false;
+        }
 
+    }
+
+    /**
+     * 验证是否是白名单中的ＩＰ或ＩＰ段
+     * @param string $ip
+     * @return bool|void
+     */
+    public function is_white_ip($ip = ""){
+        // 如果没有传$ip，就调用获取客户端ＩＰ的方法
+        if (!$ip){
+            $ip = $this->get_client_ip();
+        }
+        //读取配置 white_ip_list（配置文件中将IP或IP段构造为数组的key，因为复杂度array_key_exists比in_array是1比n）
+        $white_ip_list = KIRK::get_instance()->get_config("white_ip_list");
+        // 如果是指定ＩＰ而不是ＩＰ段
+        if (array_key_exists($ip,$white_ip_list)){
+            return true;
+        }
+
+        // 如果是符合ＩＰ段的ＩＰ，先通过“.”符号截取ＩＰ的前三组数据
+        $ip_pre = explode(".",$ip);
+        array_pop($ip_pre);
+        $ip_pre = implode(".",$ip_pre);
+        // 判断IP前缀是否在IP段内
+        if (array_key_exists($ip_pre,$white_ip_list)){
+            return true;
+        }
+
+        return ;
+    }
 }
---
+++ core/config/common.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirk
+ * Date: 18-6-19
+ * Time: 下午6:05
+ */
+
+//
+$config["white_ip_list"] = array(
+    "127.0.0.1" => "*",
+    "127.0.0" => "*",// 本地调试
+);
