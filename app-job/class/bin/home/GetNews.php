208faa1fd3c2096d40277caa16690792aace007d
ClanceyHuang <clanceyhuang@gmail.com>
2018-06-22 09:09:13 +0800

    debug job code !

--- app-job/class/bin/home/GetNews.php
+++ app-job/class/bin/home/GetNews.php
@@ -6,10 +6,11 @@
  * Date: 18-6-20
  * Time: 上午10:05
  */
-namespace Bin;
-class GetNews extends \Bin_Abstract {
+
+class Bin_Home_GetNews extends \Bin_Abstract {
 
     public function run($argv, $argc) {
         // 此处调用各平台api获取新闻
+        echo "job demo";
     }
 }
