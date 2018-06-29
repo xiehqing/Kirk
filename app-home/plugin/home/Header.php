531ad526ea1ec257e3e5d17ad005a82a7972af59
ClanceyHuang <clanceyhuang@gmail.com>
2018-06-29 16:42:43 +0800

    优化

--- app-home/plugin/home/Header.php
+++ app-home/plugin/home/Header.php
@@ -7,23 +7,10 @@ class Home_HeaderPlugin extends Plugin {
      * @return string
      */
     public function get_content() {
-//        $request = KIRK::get_instance()->get_request();
-//
-//        $is_index = $request->get_attribute('is_index');
-//        $this->set_data('is_index',$is_index?:'');
-//        // 获取顶部左侧菜单栏
-//        $left_menu_data = $this->get_menu_data(\Bll\Home\Menu::HEADER_MENU_LEFT,$limit='0,5');
-//        $this->set_data('left_menu_data',$left_menu_data);
-//        // 获取顶部右侧菜单栏按钮
-//        $right_menu_data = $this->get_menu_data(\Bll\Home\Menu::HEADER_MENU_RIGHT,$limit="0,3");
-//        $this->set_data('right_menu_data',$right_menu_data);
+
         $data = $this->get_construct_datas();
-//        foreach($data as $k=>$v) {
-//            $
-//            $this->set_data($k, $v);
-//        }
         $this->set_data('data', $data);
-//        var_dump($data);
+
         return 'Home\Header';
     }
 
