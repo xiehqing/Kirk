909b582d2b5ad18bc07242854484918ff705591b
ClanceyHuang <clanceyhuang@gmail.com>
2018-06-29 10:37:34 +0800

    job演示

--- app-job/class/bin/home/GetNews.php
+++ app-job/class/bin/home/GetNews.php
@@ -10,7 +10,18 @@
 class Bin_Home_GetNews extends \Bin_Abstract {
 
     public function run($argv, $argc) {
-        // 此处调用各平台api获取新闻
-        echo "job demo";
+        // 演示在脚本中两种方法获取数据库中相应的新闻数据
+
+        // 1、直接通过sql语句的方法
+//        $dao_home_news = new Dao_FromTable('kirk', 'tb_home_news', 'news_id');
+//        $field = "news_title,news_site_url,news_images_url,news_abs,news_content";
+//        $home_news_data = $dao_home_news->exeSQL("select $field from tb_home_news where status=1");
+
+        // 2、通过调用bll的方法
+        $bll_home_news = new \Bll\Home\News();
+        $home_news_data = $bll_home_news->get_available_news();
+
+        // 拿到新闻数据后，根据具体的业务情况再进行下一步操作，demo演示直接dump
+        var_dump($home_news_data);
     }
 }
