28d0fd2089440c63fa79774e20dd59c3b36b36e3
ClanceyHuang <clanceyhuang@gmail.com>
2018-07-04 11:32:02 +0800

    update

--- app-home/ctrl/home/Index.php
+++ app-home/ctrl/home/Index.php
@@ -11,30 +11,29 @@ class IndexCtrl extends BaseCtrl {
     public function index($params,$request){
 
 
-//        # 顶部banner图
-//        $bll_banner = new \Bll\Home\Banner();
-//        $top['banner'] = $bll_banner->get_available_banners();
-//
-//        # 侧边标签
-//        $bll_tag = new \Bll\Home\Tag();
-//        $side['tag'] = $bll_tag->get_available_tags();
-//
-//        # 侧边分类
-//        $bll_category = new \Bll\Home\Category();
-//        $side['category'] = $bll_category->get_available_categories();
-//
-//        # 文章
-//        $bll_article = new \Bll\Home\Article();
-//        $main['article'] = $bll_article->get_available_articles();
-//
-//        # 新闻动态
-//        $bll_news = new \Bll\Home\News();
-//        $main['news'] = $bll_news->get_available_news();
+        # 顶部banner图
+        $bll_banner = new \Bll\Home\Banner();
+        $top['banner'] = $bll_banner->get_available_banners();
 
-//        $request->set_attribute("top",$top);
-//        $request->set_attribute("side",$side);
-//        $request->set_attribute("main",$main);
+        # 侧边标签
+        $bll_tag = new \Bll\Home\Tag();
+        $side['tag'] = $bll_tag->get_available_tags();
 
+        # 侧边分类
+        $bll_category = new \Bll\Home\Category();
+        $side['category'] = $bll_category->get_available_categories();
+
+        # 文章
+        $bll_article = new \Bll\Home\Article();
+        $main['article'] = $bll_article->get_available_articles();
+
+        # 新闻动态
+        $bll_news = new \Bll\Home\News();
+        $main['news'] = $bll_news->get_available_news();
+
+        $request->set_attribute("top",$top);
+        $request->set_attribute("side",$side);
+        $request->set_attribute("main",$main);
 
         return 'Home\Index';
     }
